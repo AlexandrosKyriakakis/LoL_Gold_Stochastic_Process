a961ace6a846f0c83946a78e02339288b678d58c
hmmyalex@gmail.com <hmmyalex@gmail.com>
2021-05-13 17:57:39 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# League of Legends Gold Stochastic Proccess
+
+### We assume the following:
+
+- Teams with equal initial probability of wining the first trade
+- Each trade gives 0.01 probability advantage to the team won the trade and 0.01 probability disadvantage to the other team
+
+### Results
+
+
+### Running the script 
+
+```sh
+
+$ python3 lolqueue.py
+
+```
+
+### MIT LICENCE
+
+### Contributors
+Alexandros Kyriakakis
---
+++ lolqueue.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+Team_A = []
+Team_B = []
+
+trade = 1
+p = 0.5 # Probability of Team A win the trade
+q = 0.5 # Probability of Team B win the trade
+advantage = 0.01 # Trade advantage
+for i in range(100):
+   won_mini_trade = np.random.choice(["A","B"],p=[p,q])
+   if (won_mini_trade == "A"):
+      if (p < 0.99):
+         p += advantage
+         q -= advantage
+      Team_A.append(trade)
+      Team_B.append(0)
+   else :
+      if (q < 0.99):
+         p -= advantage
+         q += advantage
+      Team_B.append(trade)
+      Team_A.append(0)
+fig, ax = plt.subplots()
+ax.plot([ sum(Team_A[:i]) for i in range(1,len(Team_A))], label="Team A") # Plot the summation of gold earned at every trade
+ax.plot([ sum(Team_B[:i]) for i in range(1,len(Team_B))], label="Team B")
+ax.set(xlabel='Time', ylabel='Gold', title='League of Legends Gold Stochastic Proccess')
+ax.legend()
+ax.grid()
+plt.show()
